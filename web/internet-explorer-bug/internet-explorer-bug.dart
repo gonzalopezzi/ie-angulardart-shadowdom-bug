4b21b6454dc4455b5e2ac454ee5bc86311044d0c
gonzalopezzi <gonzalopezzi@gmail.com>
2014-06-01 19:22:36 +0200

    Even simpler code

--- web/internet-explorer-bug/internet-explorer-bug.dart
+++ web/internet-explorer-bug/internet-explorer-bug.dart
@@ -1,22 +1,9 @@
 import 'package:angular/angular.dart';
-import 'dart:html';
 
 @Component(selector: 'internet-explorer-bug', 
             templateUrl:'internet-explorer-bug/internet-explorer-bug.html', 
             cssUrl:'internet-explorer-bug/internet-explorer-bug.css',
             useShadowDom: true,
-            publishAs: 'ctrl')
-class InternetExplorerBug implements ShadowRootAware {
-  DivElement _mainDiv;
-  
-  @override
-  void onShadowRoot(ShadowRoot shadowRoot) {
-    _mainDiv = shadowRoot.querySelector ("#main-div") as DivElement;
-    _appendSomeContent();
-  }
-  
-  void _appendSomeContent() {
-    _mainDiv.append(new DivElement()..className = "red-div"
-                                    ..appendText("Red background?"));
-  }
+            publishAs: 'cmp')
+class InternetExplorerBug {
 }
